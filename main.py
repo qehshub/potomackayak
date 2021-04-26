8117bc3a922a777e6b61706370832d96ffe1e146
Andres Garcia <andresgarcia@Andress-MBP-2.fios-router.home>
2021-04-26 12:04:18 -0400

    improvements

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 def get_weather():
-    key = st.secrets["WEATHER_API_KEY"]
+    key = st.secrets["weather_api_key"]
     url = f"http://api.openweathermap.org/data/2.5/weather?lat=38.9741&lon=-77.16329&appid=d17a615c545ef1981aed6b1cb4ada570&units=imperial"
     response = requests.get(url).text
     jsonData = json.loads(response)
